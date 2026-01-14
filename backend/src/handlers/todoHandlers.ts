15eb6e2ef33935e313b731fc64350953d473eead
Keisuke <keisuke@ksk-xps13.localdomain>
2026-01-14 16:29:57 +0900

    feat: zod を導入しリクエストボディのチェックを強化

--- backend/src/handlers/todoHandlers.ts
+++ backend/src/handlers/todoHandlers.ts
@@ -1,42 +1,59 @@
 import { prisma } from "@/utils/prisma";
-import { TypedRequestBody } from "@/utils/request";
 import { Request, Response } from "express";
+import { z } from "zod";
 
 export const listTodoHandler = async (req: Request, res: Response) => {
   const rows = await prisma.todo.findMany();
   res.json(rows);
 };
 
-export const createTodoHandler = async (
-  req: TypedRequestBody<{
-    title: string;
-    description?: string;
-    statusId: number;
-  }>,
-  res: Response
-) => {
-  const statuses = await prisma.todoStatus.findMany({
-    where: {
-      id: req.body.statusId,
-    },
+export const createTodoHandler = async (req: Request, res: Response) => {
+  const schema = z.object({
+    title: z.string().min(1),
+    description: z.string().optional(),
+    statusId: z.number().int().positive(),
   });
-  if (statuses.length === 0) {
-    res.status(400).json({
-      errorMsg: `invalid statudId(${req.body.statusId})`,
+
+  try {
+    const { title, description, statusId } = schema.parse(req.body);
+
+    const statuses = await prisma.todoStatus.findMany({
+      where: {
+        id: statusId,
+      },
     });
-    return;
-  }
-  const status = statuses[0];
-  await prisma.todo.create({
-    data: {
-      title: req.body.title,
-      description: req.body.description,
-      status: {
-        connect: {
-          id: status.id,
+
+    if (statuses.length === 0) {
+      res.status(400).json({
+        errorMsg: `invalid statudId(${statusId})`,
+      });
+      return;
+    }
+
+    const status = statuses[0];
+
+    await prisma.todo.create({
+      data: {
+        title: title,
+        description: description,
+        status: {
+          connect: {
+            id: status.id,
+          },
         },
       },
-    },
-  });
-  res.status(201).send();
+    });
+
+    res.status(201).send();
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      res.status(400).json({
+        errorMsg: z.flattenError(error),
+      });
+    } else {
+      res.status(500).json({
+        errorMsg: "Internal server error",
+      });
+    }
+  }
 };
