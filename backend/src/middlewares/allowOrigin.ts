f745b20b0a8387e648eebc84a31e36336c3a6cb9
ksk-uchi <ksk.uchi+github@gmail.com>
2026-01-20 19:29:50 +0900

    feat: API の URL を変更

---
+++ backend/src/middlewares/allowOrigin.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+
+export function allowOrigin(req: Request, res: Response, next: NextFunction) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS",
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    res.sendStatus(200);
+    return;
+  }
+  next();
+}
--- frontend/app/api/todoApi.ts
+++ frontend/app/api/todoApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import type { CreateTodoDto, Todo, UpdateTodoDto } from "../types";
 
-const baseURL = "http://localhost:3000"; // Adjust based on actual backend URL if different
+const baseURL = "http://localhost:3000/todo";
 
 const api = axios.create({
   baseURL: baseURL,
@@ -13,26 +13,26 @@ type todoListApiResponse = {
 
 export const todoApi = {
   getAll: async (): Promise<Todo[]> => {
-    const response = await api.get<todoListApiResponse>("/todos");
+    const response = await api.get<todoListApiResponse>("/");
     return response.data.todo;
   },
 
   getById: async (id: number): Promise<Todo> => {
-    const response = await api.get<Todo>(`/todo/${id}`);
+    const response = await api.get<Todo>(`/${id}`);
     return response.data;
   },
 
   create: async (data: CreateTodoDto): Promise<Todo> => {
-    const response = await api.post<Todo>("/create_todo", data);
+    const response = await api.post<Todo>("/", data);
     return response.data;
   },
 
   update: async (id: number, data: UpdateTodoDto): Promise<Todo> => {
-    const response = await api.patch<Todo>(`/todo/${id}`, data);
+    const response = await api.patch<Todo>(`/${id}`, data);
     return response.data;
   },
 
   delete: async (id: number): Promise<void> => {
-    await api.delete(`/todo/${id}`);
+    await api.delete(`/${id}`);
   },
 };
