62663cffd4695a26f0731444ca9449b9bf66a1c6
ksk-uchi <ksk.uchi+github@gmail.com>
2026-01-21 17:40:01 +0900

    refactoring: todo 関連 API の構造を変更 (エラーの時例外を挙げる、内容をサービスに切り出す)

---
+++ backend/src/errors/clientSideError.ts
@@ -0,0 +1,36 @@
+import { ZodError } from "zod";
+
+export class ClientSideError extends Error {
+  status: number;
+  constructor(message: string, status = 400) {
+    super(message);
+    this.name = "ClientSideError";
+    this.status = status;
+  }
+}
+
+export class NotFoundError extends ClientSideError {
+  constructor(message: string) {
+    super(message, 404);
+    this.name = "NotFoundError";
+  }
+}
+
+export class ValidationError extends ClientSideError {
+  targets: Record<string, string>;
+  constructor(message: string, errorItems: ZodError | Record<string, string>) {
+    let targets: Record<string, string> = {};
+    if (errorItems instanceof ZodError) {
+      errorItems.issues.forEach((issue) => {
+        issue.path.forEach((path) => {
+          targets[path.toString()] = issue.code;
+        });
+      });
+    } else {
+      targets = errorItems;
+    }
+    super(message, 422);
+    this.name = "ValidationError";
+    this.targets = targets;
+  }
+}
