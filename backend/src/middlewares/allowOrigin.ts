57fa82ffd1b550ff0f83427d7e89ca15f408a4a4
ksk-uchi <ksk.uchi+github@gmail.com>
2026-01-23 15:44:47 +0900

    config(backend): allow X-CSRF-Token header in CORS

--- backend/src/middlewares/allowOrigin.ts
+++ backend/src/middlewares/allowOrigin.ts
@@ -6,7 +6,10 @@ export function allowOrigin(req: Request, res: Response, next: NextFunction) {
     "Access-Control-Allow-Methods",
     "GET, POST, PATCH, PUT, DELETE, OPTIONS",
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-CSRF-Token",
+  );
   if (req.method === "OPTIONS") {
     res.sendStatus(200);
     return;
